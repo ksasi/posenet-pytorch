315fdde06d6b4bb58c9c0c2f2e661f61967078ac
Sasikanth <sasikanth@Sasikanths-MacBook-Pro.local>
2021-03-31 22:09:09 +0530

    ibb

--- image_demo.py
+++ image_demo.py
@@ -71,7 +71,7 @@ def main():
 
                 keypoint_coords *= output_scale
                 keypoint_coords_ref *= output_scale
-                coord_list.append(keypoint_coords_ref[0]+keypoint_coords[0]+[[label,0]])
+                coord_list.append(keypoint_coords_ref[0]+keypoint_coords[0]+numpy.array([[label,0]]))
     print(coord_list)
     final_array = numpy.array(coord_list).reshape(-1,(17+17)*2)
     numpy.savetxt("coord_list.csv", final_array, delimiter=",") 
