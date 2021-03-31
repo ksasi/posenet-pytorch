5eb93a8cae3fa02a6b7dcf8b631cb9bd68d71cd3
Sasikanth <sasikanth@Sasikanths-MacBook-Pro.local>
2021-03-31 23:25:21 +0530

    cic

--- image_demo.py
+++ image_demo.py
@@ -77,10 +77,11 @@ def main():
                 #print(type(keypoint_coords))
                 #print(coord_list)
                 #coord_list.append(numpy.array([label, 0]))
-                coord_list.append(keypoint_coords_ref[0]+keypoint_coords[0] )
+                keypoint_coords_final = np.concatenate([keypoint_coords_ref[0], keypoint_coords[0], numpy.array([label, 0])])
+                coord_list.append(keypoint_coords_final)
                 #print(coord_list)
     #print(coord_list)
-    final_array = numpy.array(coord_list).reshape(-1,(17+17)*2)
+    final_array = numpy.array(coord_list).reshape(-1,(17+17+1)*2)
     numpy.savetxt("coord_list.csv", final_array, delimiter=",") 
 
 '''
